bc6b224740802938bfc0b13663f5a02de773da56
annisaputrinurhidayah <annisaputrinurhidayah@gmail.com>
2021-10-25 18:47:06 +0700

    first

--- js/script.js
+++ js/script.js
@@ -1,18 +1,43 @@
-// even pada saat link diklik
-$('.page-scroll').on('click', function(e) {
+$('.page-scroll').on('click', function(e){
+	var tujuan = $(this).attr('href');
+	var elemenTujuan = $(tujuan);
+	$('html , body').animate({
+		scrollTop: elemenTujuan.offset().top - 50
+	}, 1250);
+	e.preventDefault();
+});
 
-    // ambil isi href
-    var tujuan = $(this).attr('href')
 
-    // tangkap elemen ybs
-    var elemenTujuan = $(tujuan);
-    
-    // pindahkan scroll
-    $('body').animate({
-        scrollTop: elemenTujuan.offset().top
-    });
+// paralax
+//about
+$(window).on('load', function() {
+	$('.pKiri').addClass('pMuncul');
+	$('.pKanan').addClass('pMuncul');
+});
 
-    e.preventDefault();
+$(window).scroll(function() {
+	var wScroll = $(this).scrollTop();
 
-    
+
+	// jumbotron
+	$('.jumbotron img').css({
+		'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+	})
+	$('.jumbotron h1').css({
+		'transform' : 'translate(0px, '+ wScroll/2.1 +'%)'
+	})
+	$('.jumbotron p').css({
+		'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+	})
+
+
+
+	// portfolio
+	if( wScroll > $('.portfolio').offset().top - 180) {
+		$('.portfolio .thumbnail').each(function(i) {
+			setTimeout(function() {
+				$('.portfolio .thumbnail').eq(i).addClass('muncul');	
+			}, 300 * i+1);
+		});
+	}
 });
